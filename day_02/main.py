5ef48b5241d1cc19382a4bdf2924a7146ab706ea
Max Pot <mpot@lely.com>
2024-12-02 12:49:51 +0100

    feat(day_02/): Finished task 2 of day 2

--- day_02/main.py
+++ day_02/main.py
@@ -15,30 +15,48 @@ def get_differences_arr(list: list[int]) -> np.array:
     return np.diff(np.array(list))
 
 
-def check_safety(data: list[list[int]]) -> bool:
-    logger.info("Checking differences")
+def check_safety(data: list[int]) -> int:
+    # The levels are either all increasing or all decreasing.
+    all_positive_or_negative = np.all(data > 0) | np.all(data < 0)
+    # Any two adjacent levels differ by at least one and at most three.
+    within_range = np.all((1 <= np.abs(data)) & (np.abs(data) <= 3))
+
+    if all_positive_or_negative & within_range:
+        return 1
+    else:
+        return 0
+
+
+def check_safety_problem_dampener(data: list[int]) -> int:
     safe_reports = 0
-    for arr in data:
-        diffs = get_differences_arr(arr)
-
-        logger.info("Checking safety")
-        # The levels are either all increasing or all decreasing.
-        all_positive_or_negative = np.all(diffs > 0) | np.all(diffs < 0)
-        # Any two adjacent levels differ by at least one and at most three.
-        within_range = np.all((1 <= np.abs(diffs)) & (np.abs(diffs) <= 3))
-
-        if all_positive_or_negative & within_range:
-            safe_reports += 1
-
-    return safe_reports
+    diffs = get_differences_arr(data)
+    if check_safety(diffs):
+        return 1
+    else:
+        for index, value in enumerate(data):
+            data_2 = np.delete(data, index)
+            diffs_2 = get_differences_arr(data_2)
+            if check_safety(diffs_2):
+                return 1
+            else:
+                continue
+        return 0
 
 
 def main():
     data = import_data(file_path="./day_02/data/input.txt")
-    logger.info(f"Imported data: {data}")
+    logger.info("Imported data")
 
-    safe_reports = check_safety(data)
-    logger.info(f"Number of safe reports: {safe_reports}")
+    safe_reports = 0
+    for arr in data:
+        diffs = get_differences_arr(arr)
+        safe_reports += check_safety(diffs)
+    logger.info(f"Number of safe reports for task 1: {safe_reports}")
+
+    safe_reports = 0
+    for arr in data:
+        safe_reports += check_safety_problem_dampener(arr)
+    logger.info(f"Number of safe reports for task 2: {safe_reports}")
 
 
 if __name__ == "__main__":
--- day_02/test_day_02.py
+++ day_02/test_day_02.py
@@ -1,30 +1,26 @@
 import numpy as np
 
-from day_02.main import get_differences_arr, check_safety
+from day_02.main import get_differences_arr, check_safety, check_safety_problem_dampener
 
 
 def test_get_differences_matrix():
-    matrix = np.array(
-        [
-            [7, 6, 4, 2, 1],
-            [1, 2, 7, 8, 9],
-            [9, 7, 6, 2, 1],
-            [1, 3, 2, 4, 5],
-            [8, 6, 4, 4, 1],
-            [1, 3, 6, 7, 9],
-        ]
-    )
+    matrix = [
+        [7, 6, 4, 2, 1],
+        [1, 2, 7, 8, 9],
+        [9, 7, 6, 2, 1],
+        [1, 3, 2, 4, 5],
+        [8, 6, 4, 4, 1],
+        [1, 3, 6, 7, 9],
+    ]
 
-    test_diffs = np.array(
-        [
-            [-1, -2, -2, -1],
-            [1, 5, 1, 1],
-            [-2, -1, -4, -1],
-            [2, -1, 2, 1],
-            [-2, -2, 0, -3],
-            [2, 3, 1, 2],
-        ]
-    )
+    test_diffs = [
+        [-1, -2, -2, -1],
+        [1, 5, 1, 1],
+        [-2, -1, -4, -1],
+        [2, -1, 2, 1],
+        [-2, -2, 0, -3],
+        [2, 3, 1, 2],
+    ]
 
     for arr, diff in zip(matrix, test_diffs):
         calc_diff = get_differences_arr(arr)
@@ -32,6 +28,23 @@ def test_get_differences_matrix():
 
 
 def test_check_safety():
+    matrix = [
+        [7, 6, 4, 2, 1],
+        [1, 2, 7, 8, 9],
+        [9, 7, 6, 2, 1],
+        [1, 3, 2, 4, 5],
+        [8, 6, 4, 4, 1],
+        [1, 3, 6, 7, 9],
+    ]
+    safe_reports = 0
+    for arr in matrix:
+        diffs = get_differences_arr(arr)
+        safe_reports += check_safety(diffs)
+
+    assert safe_reports == 2
+
+
+def test_check_safety_problem_dampener():
     matrix = np.array(
         [
             [7, 6, 4, 2, 1],
@@ -42,5 +55,8 @@ def test_check_safety():
             [1, 3, 6, 7, 9],
         ]
     )
-    safe_reports = check_safety(matrix)
-    assert safe_reports == 2
+    safe_reports = 0
+    for arr in matrix:
+        safe_reports += check_safety_problem_dampener(arr)
+
+    assert safe_reports == 4
